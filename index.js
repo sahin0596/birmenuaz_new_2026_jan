d5fde60a35429b6da2cd32228206ccaddbcf046a
Sahin Nasibov <138691704+sahin0596@users.noreply.github.com>
2026-05-17 16:59:07 +0400

    refactoer index js

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname);
+
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".png": "image/png",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+  ".woff": "font/woff",
+  ".woff2": "font/woff2",
+  ".ttf": "font/ttf",
+  ".eot": "application/vnd.ms-fontobject",
+};
 
 const server = http.createServer((req, res) => {
-  res.end("Render works!");
+  let requestPath = decodeURIComponent(req.url.split("?")[0]);
+  if (requestPath === "/") {
+    requestPath = "/index.html";
+  }
+
+  const filePath = path.join(rootDir, requestPath);
+  if (!filePath.startsWith(rootDir)) {
+    res.writeHead(403, { "Content-Type": "text/plain" });
+    return res.end("Forbidden");
+  }
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      return res.end("Not Found");
+    }
+
+    if (stats.isDirectory()) {
+      const indexFile = path.join(filePath, "index.html");
+      return fs.stat(indexFile, (indexErr) => {
+        if (indexErr) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          return res.end("Not Found");
+        }
+        serveFile(indexFile, res);
+      });
+    }
+
+    serveFile(filePath, res);
+  });
 });
 
-server.listen(3000, () => {
-  console.log("Server running");
+function serveFile(filePath, res) {
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = mimeTypes[ext] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("Server Error");
+    }
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  });
+}
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
